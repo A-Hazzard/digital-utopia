f154189b73caa1183caf77d8c024b28fd86fe18c
Aaron Hazzard <62683196+A-Hazzard@users.noreply.github.com>
2024-10-16 11:12:43 -0400

    Basic Design Of Dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,6 +5,8 @@ import Image from "next/image";
 import { useState, useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../../firebase";
+import History from "@/components/History";
+
 export default function Dashboard() {
   const [isOpen, setIsOpen] = useState(false);
   const [username, setUsername] = useState("");
@@ -22,9 +24,61 @@ export default function Dashboard() {
   }, []);
 
   return (
-    <div className={`h-screen bg-background flex`}>
+    <div className={`bg-background flex h-screen`}>
+      {/* Left Panel for Desktop */}
+      <div className={`hidden lg:flex flex-col bg-darker p-4 w-64`}>
+        <div className="flex items-center mb-8">
+          <Image
+            src="/whiteLogo.svg"
+            className="w-full h-auto"
+            alt="Logo"
+            width={100}
+            height={100}
+          />
+        </div>
+
+        <ul className={`text-light flex flex-col gap-6`}>
+          <li className="flex items-center gap-2">
+            <Image
+              src="/dashboardIcon.svg"
+              alt="Dashboard"
+              width={20}
+              height={20}
+            />{" "}
+            Dashboard
+          </li>
+          <li className="flex items-center gap-2">
+            <Image
+              src="/profileIcon.svg"
+              alt="Profile"
+              width={20}
+              height={20}
+            />{" "}
+            Profile
+          </li>
+          <li className="flex items-center gap-2">
+            <Image
+              src="/paymentsIcon.svg"
+              alt="Payments"
+              width={20}
+              height={20}
+            />{" "}
+            Payments
+          </li>
+          <li className="flex items-center gap-2">
+            <Image
+              src="/invoicesIcon.svg"
+              alt="Invoices"
+              width={20}
+              height={20}
+            />{" "}
+            Invoices
+          </li>
+        </ul>
+      </div>
+
       {/* Mobile Navbar */}
-      <div className={`lg:hidden`}>
+      <div className={`absolute lg:relative lg:hidden`}>
         <button onClick={() => setIsOpen(true)} className="p-2">
           {/* Custom Sandwich SVG Icon */}
           <svg
@@ -79,39 +133,28 @@ export default function Dashboard() {
         )}
       </div>
 
-      {/* Left Panel for Desktop */}
-      <div className={`hidden lg:flex flex-col bg-darker p-4 w-64`}>
-        <div className={`flex items-center mb-8`}>
-          {/* Logo */}
-          <div>Logo</div>
-        </div>
-
-        <ul className={`flex flex-col gap-6`}>
-          <li>Dashboard</li>
-          <li>Profile</li>
-          <li>Payments</li>
-          <li>Invoices</li>
-        </ul>
-      </div>
-
       {/* Main Content */}
-      <main className={`flex flex-col gap-4 pt-10 pr-8 lg:p-8`}>
-        <div className="w-3/12">
+      <main
+        className={`flex-1 flex flex-col gap-4 pt-10 px-10 lg:p-8 overflow-y-auto`}
+      >
+        <div className="w-3/12 md:w-2/12">
           <Avatar
             src="/avatar.svg"
             alt="Dashboard Placeholder"
             className="w-full h-auto"
           />
         </div>
+
         <h1 className="text-light text-2xl font-bold">
           WELCOME BACK TO UTOPIA Mr. {username}.
         </h1>
-        <div className="flex flex-col lg:flex-row gap-4">
-          
-          <div>
+
+        <div className="flex flex-col lg:flex-row gap-4 lg:justify-between">
+          <div className="flex-grow">
             <p className="text-gray">Your Profits</p>
+
             <div className="flex items-center gap-2">
-              <span className="text-light text-2xl font-bold">
+              <span className="text-light text-2xl lg:text-xl font-bold">
                 $40,000 USDT
               </span>
               <Image
@@ -122,6 +165,7 @@ export default function Dashboard() {
                 height={15}
               />
             </div>
+
             <div className="flex items-center gap-2">
               <Image
                 src="/uptrend.svg"
@@ -134,10 +178,10 @@ export default function Dashboard() {
             </div>
           </div>
 
-          <div>
+          <div className="flex-grow">
             <p className="text-gray">Investment</p>
             <div className="flex items-center gap-2">
-              <span className="text-light text-2xl font-bold">
+              <span className="text-light text-2xl lg:text-xl font-bold">
                 $10,000 USDT
               </span>
               <Image
@@ -148,22 +192,34 @@ export default function Dashboard() {
                 height={15}
               />
             </div>
-            
           </div>
 
-          <div className="flex flex-col justify-center gap-4">
-            <Button className="flex p-6 items-center gap-2 bg-orange text-light">
-                <Image src="/plusButton.svg" alt="Plus Icon" width={20} height={20} />
-                Deposit Funds
+          <div className="flex flex-col lg:flex-row justify-center lg:justify-end gap-4 lg:gap-2 md:w-5/12">
+            <Button className="flex p-6 lg:w-full items-center gap-2 bg-orange text-light">
+              <Image
+                src="/plusButton.svg"
+                alt="Plus Icon"
+                width={20}
+                height={20}
+              />
+              Deposit Funds
             </Button>
-            <Button className="flex p-6 items-center gap-2 bg-gray text-light">
-                <Image src="/minusButton.svg" alt="Minus Icon" width={20} height={20} />
-                Deposit Funds
+            <Button className="flex p-6 lg:w-full items-center gap-2 bg-gray text-light">
+              <Image
+                src="/minusButton.svg"
+                alt="Minus Icon"
+                width={20}
+                height={20}
+              />
+              Withdraw Funds
             </Button>
           </div>
-
         </div>
         <hr className="border-gray" />
+
+        <h2 className="text-light text-xl font-bold">Your Profits</h2>
+
+        <History />
       </main>
     </div>
   );
---
+++ components/History.tsx
@@ -0,0 +1,129 @@
+import React from "react";
+import { Calendar, CheckCircle, Clock } from "lucide-react";
+
+const trades = [
+  {
+    date: "12/12/2023",
+    time: "12:47",
+    status: "Pending",
+    pair: "BTC • ETH",
+    profit: "0.00210123BTC\n140.63USD",
+  },
+  {
+    date: "12/12/2023",
+    time: "12:47",
+    status: "Pending",
+    pair: "BTC • ETH",
+    profit: "0.00210123BTC\n140.63USD",
+  },
+  {
+    date: "12/12/2023",
+    time: "12:47",
+    status: "Done",
+    pair: "BTC • ETH",
+    profit: "0.00210123BTC\n140.63USD",
+  },
+  {
+    date: "12/12/2023",
+    time: "12:47",
+    status: "Pending",
+    pair: "BTC • ETH",
+    profit: "0.00210123BTC\n140.63USD",
+  },
+  {
+    date: "12/12/2023",
+    time: "12:47",
+    status: "Done",
+    pair: "BTC • ETH",
+    profit: "0.00210123BTC\n140.63USD",
+  },
+  {
+    date: "12/12/2023",
+    time: "12:47",
+    status: "Done",
+    pair: "BTC • ETH",
+    profit: "0.00210123BTC\n140.63USD",
+  },
+  {
+    date: "12/12/2023",
+    time: "12:47",
+    status: "Done",
+    pair: "BTC • ETH",
+    profit: "0.00210123BTC\n140.63USD",
+  },
+  {
+    date: "12/12/2023",
+    time: "12:47",
+    status: "Done",
+    pair: "BTC • ETH",
+    profit: "0.00210123BTC\n140.63USD",
+  },
+];
+
+export default function History() {
+  return (
+    <div className="overflow-x-auto">
+      <table className="text-light min-w-full">
+        <thead>
+          <tr className="text-gray-400 text-xs sm:text-sm border-b border-dashed border-gray">
+            <th className="text-left p-2">Date</th>
+            <th className="text-left p-2">Status</th>
+            <th className="text-left p-2">Pair</th>
+            <th className="text-right p-2">Profit</th>
+          </tr>
+        </thead>
+        <tbody>
+          {trades.map((trade, index) => (
+            <tr key={index} className="border-b border-gray">
+              <td className="p-2">
+                <div className="flex items-center">
+                  <Calendar size={16} className="mr-2 text-gray-400" />
+                  <div>
+                    <div className="text-sm">{trade.date}</div>
+                    <div className="text-gray-400 text-xs">{trade.time}</div>
+                  </div>
+                </div>
+              </td>
+              <td className="px-2">
+                {trade.status === "Pending" ? (
+                  <div className="flex items-center text-orange-500 text-sm">
+                    <Clock size={16} className="mr-1" />
+                    Pending
+                  </div>
+                ) : (
+                  <div className="flex items-center text-green-500 text-sm">
+                    <CheckCircle size={16} className="mr-1" />
+                    Done
+                  </div>
+                )}
+              </td>
+              <td className="p-2">
+                <div className="flex items-center">
+                  <div className="relative w-6 h-6 mr-2">
+                    <img
+                      src="/btc.svg"
+                      alt="ETH"
+                      className="w-5 h-5 rounded-full absolute bottom-0 -right-2"
+                    />
+                    <img
+                      src="/eth.svg"
+                      alt="BTC"
+                      className="w-5 h-5 rounded-full absolute top-0 left-0"
+                    />
+                  </div>
+                  <span className="text-sm">{trade.pair}</span>
+                </div>
+              </td>
+              <td className="text-right py-2">
+                <div className="text-sm">{trade.profit.split("\n")[0]}</div>
+                <div className="text-gray-400 text-xs">
+                  {trade.profit.split("\n")[1]}
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
